feb6d74abc3963acb989c9a8e2e09a6d6204e2a5
Ethan Hennard <ethan.hennard@gmail.com>
2015-01-26 21:49:43 -0500

    Structure change upload.

--- Dimmer.js
+++ Dimmer.js
@@ -1,68 +1,75 @@
-var Scene = function(rows, columns) {
-	var canvas = document.getElementById('mainCanvas');;
-	var rows = rows;
-	var columns = columns;
+// Dimmer 'namespace'
+var DIMMER = DIMMER || {};
 
-	if(canvas.getContext) {
-		canvas.addEventListener('click', function(event) {
-			console.log(event.offsetX + "/" + event.offsetY);
-		});
-		this.createBoard(rows, columns);
-	}
-}
-
-Scene.prototype.createBoard = function(rows, columns) {	
-	var interval = 55;
+DIMMER.stage = {
+	scenes: [
 	
-	document.getElementById('mainCanvas').setAttribute("height", (rows * interval) - 5);
-    document.getElementById('mainCanvas').setAttribute("width", (columns * interval) - 5);
-	
-	var xMax = rows * interval;
-	var yMax = columns * interval;
-	var grid = new Array(rows * columns)
-	var counter = 0;
-	
-	for (xIndex = 0; xIndex < xMax; xIndex += interval) {
-        for (yIndex = 0; yIndex < yMax; yIndex += interval) {
-            grid[counter] = new Tile(xIndex, yIndex, false);
-			counter++;
-        }
-    }
-}
-
-var Tile = function(xIndex, yIndex, state) {
-	this.state = state;
-	
-	var ctx = document.getElementById('mainCanvas').getContext('2d');
-	this.createGameTile(ctx, xIndex, yIndex, 50, 50, 15, "9C9C9C");
-}
-
-Tile.prototype.createGameTile = function(ctx, x, y, width, height, radius, color) {
-	
-	ctx.fillStyle = color;
-	
-	ctx.beginPath();
-	ctx.moveTo(x, y+radius);
-	ctx.lineTo(x, y+height-radius);
-	ctx.quadraticCurveTo(x, y+height, x+radius, y+height);
-	ctx.lineTo(x+width-radius, y+height);
-	ctx.quadraticCurveTo(x+width, y+height, x+width, y+height-radius);
-	ctx.lineTo(x+width, y+radius);
-	ctx.quadraticCurveTo(x+width, y, x+width-radius, y);
-	ctx.lineTo(x+radius, y);
-	ctx.quadraticCurveTo(x, y, x, y+radius);
-	ctx.fill();
-}
-
-Tile.prototype.toggleState = function() {
-	console.log("toggleState called.");
-	if(this.state) {
+	],
+	enterScene: function() { 
 		
-	} else {
+	},
+	createScene: function() {
 	
 	}
 }
 
+DIMMER.scene = {
+	objects: [
+		
+	],
+	initCanvas: function() { //Is there a way to implement this method to set a variable of an object directly to a statement? Or does it have to be done with a function like I have it? Is the method implementation better?
+	this.ctx = document.getElementById('mainCanvas').getContext('2d');
+	},
+	add: function(obj) {
+		objects[0] = Object.create(DIMMER.gameboard);
+	},
+	update: function() { //TODO: Iterates through objects list and causes the draw method to function.
+		this.objects[0].draw(this.ctx, 0, 0, 50, 50, 15, "9C9C9C");
+	}
+}
+
+
+DIMMER.gameboard = {
+	//Does it make sense to have this hierarchy? If i'm going to do anything on each tile I'll have to call from the scene to the objects array and then to the gameboard object to its boardObjects array. Is there an more simple way to implement this?
+	boardObjects = [
+		
+	],
+	draw: function(grid) = { //How do I get the number of tiles I want to implement a 3x3 board or higher to the actual gameboard object?
+		
+	}
+}
+DIMMER.tile = {
+	state: false,
+	draw: function(canvas, x, y, width, height, radius, color) {
+		canvas.fillStyle = color;
+		canvas.beginPath();
+		canvas.moveTo(x, y+radius);
+		canvas.lineTo(x, y+height-radius);
+		canvas.quadraticCurveTo(x, y+height, x+radius, y+height);
+		canvas.lineTo(x+width-radius, y+height);
+		canvas.quadraticCurveTo(x+width, y+height, x+width, y+height-radius);
+		canvas.lineTo(x+width, y+radius);
+		canvas.quadraticCurveTo(x+width, y, x+width-radius, y);
+		canvas.lineTo(x+radius, y);
+		canvas.quadraticCurveTo(x, y, x, y+radius);
+		canvas.fill();
+	},
+	toggle: function() {
+	
+	}
+}
 function init() {
-	var gameStage = new Scene(3, 3);
+	var mainStage = Object.create(DIMMER.scene);
+	mainStage.add(Object.create(DIMMER.tile));
+	mainStage.initCanvas();
+	mainStage.update();
+	debugger;
+}
+
+if (typeof Object.create !== 'function') {
+	Object.create = function (o) {
+		var F = function () {};
+		F.prototype = o;
+		return new F();
+	}
 }
